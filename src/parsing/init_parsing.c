5f2fc4b006d921d90ddb280e439b995ebdd6d8b5
Marius Alaimo <malaimo@f4r4s6.clusters.42paris.fr>
2026-03-16 11:01:13 +0100

    obligfe

--- src/parsing/init_parsing.c
+++ src/parsing/init_parsing.c
@@ -4,7 +4,7 @@ void    init_parsing(t_data *data, char **envp)
 {
 
     (void)envp;
-    // if (ft_strcmp(data->str[0], "here_doc") == 0)
-    //     parsing_heredoc(data);
-    exec(data->str[1], envp);
+    if (ft_strcmp(data->str[0], "here_doc") == 0)
+        parsing_heredoc(data);
+    // exec(data->str[1], envp);
 }
