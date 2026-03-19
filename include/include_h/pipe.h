81aad3747eda9d88d69107754897457583cafa89
jdelmott <jdelmott@f1br8s8.paris.42.school>
2026-03-19 18:08:06 +0100

    oui je jure

--- include/include_h/pipe.h
+++ include/include_h/pipe.h
@@ -6,7 +6,7 @@
 /*   By: jdelmott <jdelmott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 10:47:57 by jdelmott          #+#    #+#             */
-/*   Updated: 2026/03/19 17:02:15 by jdelmott         ###   ########.fr       */
+/*   Updated: 2026/03/19 18:07:08 by jdelmott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ void					count_pipe(t_data *data);
 int						do_pipe(t_data *data, int i);
 int						last_pipe(char *cmd, t_data *data, int outfile);
 int						last_last_pipe(t_data *data);
-// void					test_parsing_pipe(t_data *data, int i, char *envp[]);
-// void					pipe_handler(t_data *data, char *envp[], int i);
-// void					maybe_pipe(t_data *data, char *envp[]);
+char					*rm_nl_gc(char *old, t_data *data);
 
 #endif
