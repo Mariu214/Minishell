329bdb348a7c54fafaa4bb7f7d095ae8057544bf
malaimo <malaimo@f1br9s8.paris.42.school>
2026-03-19 17:14:01 +0100

    ajout fonctions en attentes

--- fonctions_en_attentes.c
+++ fonctions_en_attentes.c
@@ -1,3 +1,6 @@
+// flemme de faire un truc constructifs, ca cest bien cest simple
+
+
 void   search_pwd(char **envp)
 {
     int i;
@@ -6,4 +9,13 @@ void   search_pwd(char **envp)
     while (envp[i] && !ft_strnstr(envp[i], "PWD", 1000))
         i++;
     printf("%s\n", ft_strnstr(envp[i], "PWD", 1000));
+}
+
+void   env(char **envp)
+{
+    int i;
+
+    i = 0;
+    while (envp[i])
+        printf("%s\n", envp[i]);
 }
