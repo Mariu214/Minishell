dec168d00de4d595bf88db59b3b60daa5588b4b5
malaimo <malaimo@f1r11s19.paris.42.school>
2026-03-30 11:39:58 +0200

    modification export + ajout dollar

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: malaimo <malaimo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 09:16:41 by malaimo           #+#    #+#             */
-/*   Updated: 2026/03/30 11:38:10 by malaimo          ###   ########.fr       */
+/*   Updated: 2026/03/30 11:39:51 by malaimo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,19 +29,19 @@ int	main(int argc, char *argv[], char *envp[])
 	line = NULL;
 	while (ft_strcmp(line, "exit") != 0)
 	{
-		// init_signal(&data.sig_int, &data.sig_quit, &data.sig_child,
-		// 	&data.sig_child_slash);
-		// sigaction(SIGINT, &data.sig_int, NULL);
-		// sigaction(SIGQUIT, &data.sig_quit, NULL);
-		// line = readline(">minishell ");
-		// if (!line)
-		// 	ft_error_gc("exit\n", &data.gc, 0);
-		// if (line[0])
-		// 	add_history(line);
-		// process_running = 1;
-		// data.str = ft_split_gc(line, ' ', &data.gc);
-		// data.dollar = parsing(&data);
-		// process_running = 0;
+		init_signal(&data.sig_int, &data.sig_quit, &data.sig_child,
+			&data.sig_child_slash);
+		sigaction(SIGINT, &data.sig_int, NULL);
+		sigaction(SIGQUIT, &data.sig_quit, NULL);
+		line = readline(">minishell ");
+		if (!line)
+			ft_error_gc("exit\n", &data.gc, 0);
+		if (line[0])
+			add_history(line);
+		process_running = 1;
+		data.str = ft_split_gc(line, ' ', &data.gc);
+		data.dollar = parsing(&data);
+		process_running = 0;
 	}
 	ft_free_all_gc(&data.gc);
 	return (0);
