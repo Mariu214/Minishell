f9b06e24a9ad98b8f6ef5e31b41d917a9c2f2922
jdelmott <jdelmott@f1br8s1.paris.42.school>
2026-04-09 15:50:01 +0200

    yo le rap

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: malaimo <malaimo@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jdelmott <jdelmott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 14:57:35 by malaimo           #+#    #+#             */
-/*   Updated: 2026/04/09 12:40:16 by malaimo          ###   ########.fr       */
+/*   Updated: 2026/04/09 12:42:05 by jdelmott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -150,5 +150,6 @@ int test_lexer(t_data *data)
     int result;
 
     result = lexer(data);
+    ft_print_list(data->list);
     return (result);
 }
