f2aa353faef2c831a956505be09726b98caae53d
Marius Alaimo <malaimo@f2r5s3.clusters.42paris.fr>
2026-04-13 17:24:20 +0200

    debut parsig built in

--- src/hardcoded/export.c
+++ src/hardcoded/export.c
@@ -1,89 +1,101 @@
-// #include "../../include/minishell.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malaimo <malaimo@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/13 17:10:04 by malaimo           #+#    #+#             */
+/*   Updated: 2026/04/13 17:10:05 by malaimo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-// char    **export(char *envp[], char *str)
-// {
-//     int     i;
-//     char    **cpy;
-//     char    *temp;
-//     char    *temp2;
-//     int     j;
+#include "../../include/minishell.h"
 
-//     i = 0;
-//     j = 0;
-//     if (!envp)
-//         return (printf("env is cleared"), NULL);
-//     if (!str)
-//     {
-//         while (envp[i])
-//         	printf("declare -x %s\n", envp[i++]);
-//         return (envp);
-//     }
-//     while (str[i])
-//     {
-//         if (str[i++] == '=')
-//             j = 1;
-//     }
-//     if (j == 0)
-//         return (envp);
-//     i = 0;
-//     while (envp[i])
-//     {
-//         j = 0;
-//         while (envp[i][j] && envp[i][j] != '=')
-//             j++;
-//         temp = ft_substr(envp[i], 0, j);
-//         j = 0;
-//         while (str[j] && str[j] != '=')
-//             j++;
-//         temp2 = ft_substr(str, 0, j);
-//         if (temp && temp2)
-//         {
-//             if (strcmp(temp, temp2) == 0)
-//             {
-//                 free(envp[i]);
-//                 free(temp);
-//                 envp[i] = ft_strdup(str);
-//                 return (envp);
-//             }
-//             free(temp);
-//             i++;
-//         }
-//     }
-//     i = 0;
-//     cpy = ft_calloc(sizeof(char *), (ft_splitlen(envp) + 2));
-//     while (envp[i])
-//     {
-//         cpy[i] = ft_strdup(envp[i]);
-//         i++;
-// 	}
-//     cpy[i] = ft_strdup(str);
-//     free_tab(envp);
-//     return (cpy);
-// }
+char    **export(char *envp[], char *str)
+{
+    int     i;
+    char    **cpy;
+    char    *temp;
+    char    *temp2;
+    int     j;
 
-// int init_export(t_data *data, int i)
-// {
-//     char    **temp;
-//     int     j;
+    i = 0;
+    j = 0;
+    if (!envp)
+        return (printf("env is cleared"), NULL);
+    if (!str)
+    {
+        while (envp[i])
+        	printf("declare -x %s\n", envp[i++]);
+        return (envp);
+    }
+    while (str[i])
+    {
+        if (str[i++] == '=')
+            j = 1;
+    }
+    if (j == 0)
+        return (envp);
+    i = 0;
+    while (envp[i])
+    {
+        j = 0;
+        while (envp[i][j] && envp[i][j] != '=')
+            j++;
+        temp = ft_substr(envp[i], 0, j);
+        j = 0;
+        while (str[j] && str[j] != '=')
+            j++;
+        temp2 = ft_substr(str, 0, j);
+        if (temp && temp2)
+        {
+            if (strcmp(temp, temp2) == 0)
+            {
+                free(envp[i]);
+                free(temp);
+                envp[i] = ft_strdup(str);
+                return (envp);
+            }
+            free(temp);
+            i++;
+        }
+    }
+    i = 0;
+    cpy = ft_calloc(sizeof(char *), (ft_splitlen(envp) + 2));
+    while (envp[i])
+    {
+        cpy[i] = ft_strdup(envp[i]);
+        i++;
+	}
+    cpy[i] = ft_strdup(str);
+    free_tab(envp);
+    return (cpy);
+}
 
-//     temp = ft_split(data->line[i].str, ' ');
-//     j = 1;
-//     if (!temp[j])
-//     {
-//         export(data->env, NULL);
-//         free(temp);
-//         return (0);
-//     }
-//     while(temp[j])
-//     {
-//         data->env = export(data->env, temp[j++]);
-//         if (!data->env)
-//         {
-//             free(temp);
-//             return (-1);
-//         }
-//     }
-//     free(temp);
-//     return (0);
-// }
+int init_export(t_data *data, int i)
+{
+    char    **temp;
+    int     j;
+
+    temp = ft_split(data->line[i].str, ' ');
+    j = 1;
+    if (!temp[j])
+    {
+        export(data->env, NULL);
+        free(temp);
+        return (0);
+    }
+    while(temp[j])
+    {
+        data->env = export(data->env, temp[j++]);
+        if (!data->env)
+        {
+            free(temp);
+            return (-1);
+        }
+    }
+    free(temp);
+    return (0);
+}
 
--- src/parsing/parsing_built_in.c
+++ src/parsing/parsing_built_in.c
@@ -6,18 +6,85 @@
 /*   By: malaimo <malaimo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 13:27:44 by malaimo           #+#    #+#             */
-/*   Updated: 2026/04/13 15:46:11 by malaimo          ###   ########.fr       */
+/*   Updated: 2026/04/13 17:07:04 by malaimo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+#rajouter une categorie dans la liste chaine pour savoir si il y a erreur ou pas
+
 int parser_export(t_lexst **list)
 {
-    while ((*list)->next && (*list)->next->type == BUILT_IN)
+    int i;
+    
+    *list = (*list)->next;
+    if (*list && (*list)->type == BUILT_IN)
     {
-        if (ft_strchr((*list)->content, '-'))
+        i = 0;
+        if ((*list)->content[0] == '-')
+            return (printf("minishell: export: `%s': no options are supported", (*list)->content), 1);
+        if ((!ft_isdigit((*list)->content[0]) && (*list)->content[0] != '_')
+            || ft_strchr((*list)->content, '-'))
             return (printf("minishell: export: `%s': not a valid identifier", (*list)->content), 1);
+        while ((*list)->content[i])
+        {
+            if (!ft_isalnum((*list)->content[i]) && (*list)->content[i] != '_')
+                return (printf("minishell: export: `%s': not a valid identifier", (*list)->content), 1);
+            i++;
+        }
+        *list = (*list)->next;
+    }
+    while (*list && (*list)->type == BUILT_IN)
+    {
+        i = 0;
+        if ((!ft_isdigit((*list)->content[0]) && (*list)->content[0] != '_')
+            || ft_strchr((*list)->content, '-'))
+            return (printf("minishell: export: `%s': not a valid identifier", (*list)->content), 1);
+        while ((*list)->content[i])
+        {
+            if (!ft_isalnum((*list)->content[i]) && (*list)->content[i] != '_')
+                return (printf("minishell: export: `%s': not a valid identifier", (*list)->content), 1);
+            i++;
+        }
+        *list = (*list)->next;
+    }
+    return (0);
+}
+
+int parser_export(t_lexst **list)
+{
+    int i;
+    
+    *list = (*list)->next;
+    if (*list && (*list)->type == BUILT_IN)
+    {
+        i = 0;
+        if ((*list)->content[0] == '-')
+            return (printf("minishell: export: `%s': no options are supported", (*list)->content), 1);
+        if ((!ft_isdigit((*list)->content[0]) && (*list)->content[0] != '_')
+            || ft_strchr((*list)->content, '-'))
+            return (printf("minishell: export: `%s': not a valid identifier", (*list)->content), 1);
+        while ((*list)->content[i])
+        {
+            if (!ft_isalnum((*list)->content[i]) && (*list)->content[i] != '_')
+                return (printf("minishell: export: `%s': not a valid identifier", (*list)->content), 1);
+            i++;
+        }
+        *list = (*list)->next;
+    }
+    while (*list && (*list)->type == BUILT_IN)
+    {
+        i = 0;
+        if ((!ft_isdigit((*list)->content[0]) && (*list)->content[0] != '_')
+            || ft_strchr((*list)->content, '-'))
+            return (printf("minishell: export: `%s': not a valid identifier", (*list)->content), 1);
+        while ((*list)->content[i])
+        {
+            if (!ft_isalnum((*list)->content[i]) && (*list)->content[i] != '_')
+                return (printf("minishell: export: `%s': not a valid identifier", (*list)->content), 1);
+            i++;
+        }
         *list = (*list)->next;
     }
     return (0);
@@ -26,5 +93,7 @@ int parser_export(t_lexst **list)
 void    parser_built_in(t_lexst **list)
 {
     if (ft_strcmp((*list)->content , "export") == 0)
+        parser_export(list);
+    if (ft_strcmp((*list)->content , "unset") == 0)
         parser_export(list);
 }
