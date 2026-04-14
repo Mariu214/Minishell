16e1518db0aaacfaeffab026308666a9a75c255b
jdelmott <jdelmott@f1br6s8.paris.42.school>
2026-04-14 11:25:09 +0200

    lexer is now majestic

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,13 +6,13 @@
 /*   By: jdelmott <jdelmott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 14:57:35 by malaimo           #+#    #+#             */
-/*   Updated: 2026/04/13 15:02:29 by jdelmott         ###   ########.fr       */
+/*   Updated: 2026/04/14 11:10:59 by jdelmott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-int		lexing_word(t_data *data, int *i)
+int		lexing_word(t_data *data, int *i, t_lexst **list)
 {
 	int     j;
     char    *temp;
@@ -25,11 +25,11 @@ int		lexing_word(t_data *data, int *i)
     if (!temp)
         return (1);
     *i = j;
-    ft_add_node(&data->list, temp, define_type(WORD, WRD), &data->gc);
+    ft_add_node(list, temp, define_type(WORD, WRD), &data->gc);
     return (0);
 }
 
-int		lexing_redirection(t_data *data, int *i)
+int		lexing_redirection(t_data *data, int *i, t_lexst **list)
 {
     int     j;
     char    *temp;
@@ -40,7 +40,7 @@ int		lexing_redirection(t_data *data, int *i)
     temp = ft_substr_gc(data->str, *i, j - *i, &data->gc);
     if (!temp)
         return (1);
-    if (lexing_precise_redirection(data, temp))
+    if (lexing_precise_redirection(data, temp, list))
         return (1);
     if (data->str[j] && data->str[j] == ' ')
 		j++;
@@ -49,14 +49,14 @@ int		lexing_redirection(t_data *data, int *i)
     while (data->str[*i] && data->str[*i] != '>' && data->str[*i] != '<'
 			&& data->str[*i] != '|')
 	{
-        if (lexing_sort(data, i, j))
+        if (lexing_sort(data, i, j, list))
             return (1);
         j++;
 	}
     return (0);
 }
 
-int		lexing_pipe(t_data *data, int *i)
+int		lexing_pipe(t_data *data, int *i, t_lexst **list)
 {
     int     j;
     char    *temp;
@@ -68,49 +68,48 @@ int		lexing_pipe(t_data *data, int *i)
     if (!temp)
         return (1);
     *i = j;
-    ft_add_node(&data->list, temp, define_type(PIPE, WRD), &data->gc);
+    ft_add_node(list, temp, define_type(PIPE, WRD), &data->gc);
     return (0);
 }
 
-int    lexer(t_data *data)
+int    lexer(t_data *data, t_lexst **list)
 {
     int i;
-    data->list = NULL;
 
     i = 0;
     while (data->str[i])
     {
         if (data->str[i] == '|')
         {
-            if (lexing_pipe(data, &i))
+            if (lexing_pipe(data, &i, list))
                 return (1);
         }
         else if (data->str[i] == '<' || data->str[i] == '>')
         {
-            if (lexing_redirection(data, &i))
+            if (lexing_redirection(data, &i, list))
                 return (1);
         }
 		else if (data->str[i] == '"')
         {
-            if (lexing_d_quote(data, &i, CMD))
+            if (lexing_d_quote(data, &i, CMD, list))
                 return (1);
         }
         else if (data->str[i] == '\'')
         {
-            if (lexing_s_quote(data, &i, CMD))
+            if (lexing_s_quote(data, &i, CMD, list))
                 return (1);
         }
         else if (data->str[i] == ' ')
             i++;
 		else if (data->str[i])
 		{
-			if (lexing_cmd(data, &i))
+			if (lexing_cmd(data, &i, list))
 				return (1);
 		}
     }
     while (data->list->previous)
         data->list = data->list->previous;
-    lexing_built_in(data);
+    lexing_built_in(list);
     return (0);
 }
 
@@ -118,7 +117,9 @@ int test_lexer(t_data *data)
 {
     int result;
 
-    result = lexer(data);
-    // ft_print_list(data->list);
+    data->list = NULL;
+    
+    result = lexer(data, &data->list);
+    ft_print_list(data->list);
     return (result);
 }
--- src/lexer/lexer_redirections.c
+++ src/lexer/lexer_redirections.c
@@ -6,39 +6,39 @@
 /*   By: jdelmott <jdelmott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/10 11:24:07 by malaimo           #+#    #+#             */
-/*   Updated: 2026/04/11 12:05:15 by jdelmott         ###   ########.fr       */
+/*   Updated: 2026/04/14 11:10:17 by jdelmott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
 
-int     lexing_sort(t_data *data, int *i, int jsp)
+int     lexing_sort(t_data *data, int *i, int jsp, t_lexst **list)
 {
     if (data->str[*i] == '"')
     {
-        if (lexing_d_quote(data, i, WORD))
+        if (lexing_d_quote(data, i, WORD, list))
             return (1);
     }
     else if (data->str[*i] == '\'')
     {
-        if (lexing_s_quote(data, i, WORD))
+        if (lexing_s_quote(data, i, WORD, list))
             return (1);
     }
     else if (jsp == 0)
     {
-        if (lexing_word(data, i))
+        if (lexing_word(data, i, list))
             return (1);
     }
     else if (jsp >= 1)
     {
-        if (lexing_cmd(data, i))
+        if (lexing_cmd(data, i, list))
             return (1);
     }
     return (0);
 }
 
-int     lexing_precise_redirection(t_data *data, char *str)
+int     lexing_precise_redirection(t_data *data, char *str, t_lexst **list)
 {
     int i;
 
@@ -47,25 +47,25 @@ int     lexing_precise_redirection(t_data *data, char *str)
     {
         if (str[i] && str[i + 1] && str[i] == '<' && str[i + 1] == '<')
         {
-            if (ft_add_node(&data->list, ft_substr_gc(str, i, 2, &data->gc), define_type(HEREDOC, WRD), &data->gc))
+            if (ft_add_node(list, ft_substr_gc(str, i, 2, &data->gc), define_type(HEREDOC, WRD), &data->gc))
                 return (1);
             i += 2;
         }
         else if ((str[i] && str[i + 1] && str[i] == '>' && str[i + 1] == '>'))
         {
-            if (ft_add_node(&data->list, ft_substr_gc(str, i, 2, &data->gc), define_type(OU_APPEND, WRD), &data->gc))
+            if (ft_add_node(list, ft_substr_gc(str, i, 2, &data->gc), define_type(OU_APPEND, WRD), &data->gc))
                 return (1);
             i += 2;
         }
         else if (str[i] && str[i] == '<')
         {
-            if (ft_add_node(&data->list, ft_substr_gc(str, i, 1, &data->gc), define_type(INPUT, WRD), &data->gc))
+            if (ft_add_node(list, ft_substr_gc(str, i, 1, &data->gc), define_type(INPUT, WRD), &data->gc))
                 return (1);
             i++;
         }
         else if (str[i] && str[i] == '>')
         {
-            if (ft_add_node(&data->list, ft_substr_gc(str, i, 1, &data->gc), define_type(OU_TRUNC, WRD), &data->gc))
+            if (ft_add_node(list, ft_substr_gc(str, i, 1, &data->gc), define_type(OU_TRUNC, WRD), &data->gc))
                 return (1);
             i++;
         }
