be3e2c947ef45352a6431f44462e6c0adc831ea5
Marius Alaimo <malaimo@f2r8s4.clusters.42paris.fr>
2026-04-22 09:35:55 +0200

    fedora cest tellement de la merde mais spice girl est style:

--- include/include_h/list.h
+++ include/include_h/list.h
@@ -1,19 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   list.h                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: malaimo <malaimo@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/08 16:43:10 by malaimo           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2026/04/21 11:05:13 by malaimo          ###   ########.fr       */
-=======
-/*   Updated: 2026/04/21 15:45:24 by malaimo          ###   ########.fr       */
->>>>>>> marius
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef LIST_H
 # define LIST_H
 
@@ -29,8 +13,5 @@ int					ft_delone(t_data *data, t_lexst **target);
 
 /* PRINT_LIST */
 void				ft_print_list(t_lexst *lexer);
-
-int	do_redirection(t_type redir, t_lexst *file, t_data *data);
-int	schr_redirection(t_lexst **list, t_data *data);
 
 #endif
--- include/include_h/redirection.h
+++ include/include_h/redirection.h
@@ -6,7 +6,7 @@
 /*   By: malaimo <malaimo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 14:27:35 by jdelmott          #+#    #+#             */
-/*   Updated: 2026/04/21 15:49:55 by malaimo          ###   ########.fr       */
+/*   Updated: 2026/04/22 09:35:26 by malaimo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@
 int		open_file(char *fd_arg, int mode);
 
 /* REDIRECTION */
-void	input_redirection(char *file1);
-void	output_redirection_trunc(char *file);
-void	do_redirection(t_data *data, int i);
-void	output_redirection_append(char *file);
-void	schr_redirection(t_data *data, int i);
+int 	input_redirection(char *file1);
+int 	output_redirection_append(char *file);
+int	    do_redirection(t_type redir, t_lexst *file, t_data *data);
+int	    output_redirection_trunc(char *file);
+int	    schr_redirection(t_lexst **list, t_data *data);
 
 #endif
