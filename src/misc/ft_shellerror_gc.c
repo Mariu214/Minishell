2b54e25cb4bac40c35c3c2a8e3ab5edb06e30a04
Jona Delmotte <jdelmott@f2r10s17.clusters.42paris.fr>
2026-04-28 16:31:21 +0200

    je suis fou amoureu de quelqu'un mais je vais dire qui

--- src/misc/ft_shellerror_gc.c
+++ src/misc/ft_shellerror_gc.c
@@ -6,7 +6,7 @@
 /*   By: jdelmott <jdelmott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 09:18:04 by jdelmott          #+#    #+#             */
-/*   Updated: 2026/04/28 15:32:41 by jdelmott         ###   ########.fr       */
+/*   Updated: 2026/04/28 16:30:09 by jdelmott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,9 @@ int	ft_shellerror_gc(char *str, t_data *data, int out, int mode)
 {
 	ft_printf_fd(2, "%s", str);
 	ft_free_all_gc(&data->gc);
-	if (data->pipe_heredoc[0] != -1)
-		close(data->pipe_heredoc[0]);
-	if (data->pipe_heredoc[1] != -1)
-		close(data->pipe_heredoc[1]);
-	data->pipe_heredoc[0] = -1;
-	data->pipe_heredoc[1] = -1;
+	close_fds(data);
 	if (mode == 0)
-	{		
-		close(data->old_stdin);
-		close(data->old_stdout);
+	{	
 		close(1);
 		close(0);
 		close(2);
@@ -34,4 +27,18 @@ int	ft_shellerror_gc(char *str, t_data *data, int out, int mode)
 	}
 	else
 		return (out);
+}
+
+int	close_fds(t_data *data)
+{
+	if (data->pipe_heredoc[0] != -1)
+		close(data->pipe_heredoc[0]);
+	if (data->pipe_heredoc[1] != -1)
+		close(data->pipe_heredoc[1]);
+	data->pipe_heredoc[0] = -1;
+	data->pipe_heredoc[1] = -1;
+	close(data->old_stdin);
+	close(data->old_stdout);
+
+	return (1);
 }
