587130d7d9424e8be3918ec939a82ed6e8cd047f
malaimo <malaimo@f0r3s4.paris.42.school>
2026-04-29 14:42:51 +0200

    niaque

--- src/lexer/lexer_quotes.c
+++ src/lexer/lexer_quotes.c
@@ -6,7 +6,7 @@
 /*   By: malaimo <malaimo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 12:22:25 by jdelmott          #+#    #+#             */
-/*   Updated: 2026/04/29 11:13:58 by malaimo          ###   ########.fr       */
+/*   Updated: 2026/04/29 14:42:38 by malaimo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,8 @@ int     lexing_d_quote(t_data *data, int *i, t_type type, t_lexst **list)
     {
         if (data->str[j] && data->str[j] == '"')
             num++;
-        if (num && num % 2 == 0 && data->str[j] && (data->str[j]  == ' ' || data->str[j] == '|' || data->str[j] == '<' || data->str[j] == '>'))
+        if (num && num % 2 == 0 && data->str[j] && (data->str[j]  == ' ' || data->str[j] == '|' 
+            || data->str[j] == '<' || data->str[j] == '>' || data->str[j] == '\''))
             break ;
         j++;
     }
@@ -115,7 +116,8 @@ int     lexing_s_quote(t_data *data, int *i, t_type type, t_lexst **list)
     {
         if (data->str[j] && data->str[j] == '\'')
             num++;
-        if (num && num % 2 == 0 && data->str[j] && (data->str[j]  == ' ' || data->str[j] == '|' || data->str[j] == '<' || data->str[j] == '>'))
+        if (num && num % 2 == 0 || (data->str[j] && (data->str[j]  == ' ' || data->str[j] == '|' 
+            || data->str[j] == '<' || data->str[j] == '>' || data->str[j] == '"')))
             break ;
         j++;
     }
