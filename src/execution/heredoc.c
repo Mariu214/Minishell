85df0ff8779511da26766452634b71241cea3de3
jdelmott <jdelmott@f0r4s5.paris.42.school>
2026-05-07 17:41:45 +0200

    jejejej'

--- src/execution/heredoc.c
+++ src/execution/heredoc.c
@@ -6,12 +6,60 @@
 /*   By: jdelmott <jdelmott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 11:36:24 by jdelmott          #+#    #+#             */
-/*   Updated: 2026/05/07 16:29:24 by jdelmott         ###   ########.fr       */
+/*   Updated: 2026/05/07 17:40:04 by jdelmott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include <termios.h>
+
+static void	expand_heredoc(char **str, t_data *data)
+{
+	int	i;
+	char	*temp;
+	char	*v;
+	char	*getv;
+
+	i = 0;
+	temp = ft_strdup_gc("", &data->gc);
+	getv = ft_strdup_gc("", &data->gc);
+	v = ft_strdup_gc("", &data->gc);
+	if (data->list->next && data->list->next->word_type != WRD)
+		return ;
+	while ((*str)[i])
+	{
+		if ((*str)[i] == '$')
+		{
+			i++;
+			if ((*str)[i] == '?')
+			{
+				temp = ft_renew_gc(temp, ft_itoa_gc(data->dollar, &data->gc), 0, &data->gc);
+				i++;
+			}
+			else
+			{
+				while ((*str[i] && (*str)[i] != ' ' && (*str)[i] != '?' && (*str)[i] != '$'))
+				{
+					v = ft_renew_one_gc(v, (*str)[i], &data->gc);
+					i++;
+					ft_printf_fd(2, "%c\n", (*str)[i]);
+				}
+				getv = ft_getenv_gc(v, data->env, &data->gc);
+				if (!getv)
+				{
+					ft_delone_gc((*str), &data->gc);
+					(*str) = ft_strdup_gc(temp, &data->gc);
+					return ;
+				}
+				temp = ft_renew_gc(temp, getv, 0, &data->gc);
+			}
+		}
+		temp = ft_renew_one_gc(temp, (*str)[i], &data->gc);
+		i++;
+	}
+	ft_delone_gc((*str), &data->gc);
+	(*str) = ft_strdup_gc(temp, &data->gc);
+}
 
 static char	*here_doc_next(char *lim, t_data *data)
 {
@@ -35,7 +83,8 @@ static char	*here_doc_next(char *lim, t_data *data)
 		scan = ft_scan_gc("heredoc> ", 1, &data->gc, data->old_stdin);
 		if (!scan || ft_strlen(scan) == 0)
 			ft_shellerror_gc("\n", data, 0, 0);
-		else if (ft_strcmp(scan, nl) != 0)
+		expand_heredoc(&scan, data);
+		if (ft_strcmp(scan, nl) != 0)
 			doc = ft_renew_gc(doc, scan, 0, &data->gc);
 	}
 	return (doc);
