001b73dc80974102d0f5e985f595773a05f365e7
malaimo <malaimo@f1br1s7.paris.42.school>
2026-05-11 19:04:15 +0200

    asdadawdzizi

--- src/hardcoded/print_pwd.c
+++ src/hardcoded/print_pwd.c
@@ -6,7 +6,7 @@
 /*   By: malaimo <malaimo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 14:19:24 by malaimo           #+#    #+#             */
-/*   Updated: 2026/04/30 10:58:06 by malaimo          ###   ########.fr       */
+/*   Updated: 2026/05/11 18:32:07 by malaimo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	print_pwd(t_data *data, t_lexst **list)
 {
 	*list = (*list)->next;
 	if (!getcwd(data->current_dir, 4096))
-		return (perror("error"), 1);
+		printf("%s\n", ft_getenv_gc("OLDPWD", data->env, &data->gc));
 	else
 		printf("%s\n", data->current_dir);
 	return (0);
