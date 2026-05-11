6076e18437f01f4ce470269362eaf6ab0221ea2f
malaimo <malaimo@f1br1s7.paris.42.school>
2026-05-11 20:14:44 +0200

    sdjkalkwdjawoidhaiwudhal.

--- src/hardcoded/cd.c
+++ src/hardcoded/cd.c
@@ -6,7 +6,7 @@
 /*   By: malaimo <malaimo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 14:09:17 by malaimo           #+#    #+#             */
-/*   Updated: 2026/05/11 18:21:42 by malaimo          ###   ########.fr       */
+/*   Updated: 2026/05/11 20:14:07 by malaimo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	cd(t_data *data, char *str)
 	temp = getcwd(data->current_dir, 4096);
 	if (!temp)
 		return (ft_printf_fd(2, "Minishell: couldn't access directory", 1));
-	old_temp = getenv("PWD");
+	old_temp = ft_getenv("PWD", data->env);
 	temp = ft_strjoin("PWD=", temp);
 	if (!temp)
 		return (1);
@@ -40,13 +40,42 @@ int	cd(t_data *data, char *str)
 	return (return_value);
 }
 
+int	cd_minus(t_data *data, int return_value)
+{
+	char	*temp;
+	char	*old_temp;
+
+	temp = ft_getenv("OLDPWD", data->env);
+	if (!temp)
+		return (0);
+	if (chdir(temp) == -1)
+		return (ft_printf_fd(2, "minishell: cd: %s: %s\n", temp,
+				strerror(errno)), 1);
+	temp = getcwd(data->current_dir, 4096);
+	if (!temp)
+		return (ft_printf_fd(2, "Minishell: couldn't access directory", 1));
+	old_temp = ft_getenv("PWD", data->env);
+	temp = ft_strjoin("PWD=", temp);
+	if (!temp)
+		return (1);
+	old_temp = ft_strjoin("OLDPWD=", old_temp);
+	if (!old_temp)
+		return (free(temp), 1);
+	return_value = export(data, temp);
+	if (return_value)
+		return (free(temp), free(old_temp), return_value);
+	return_value = export(data, old_temp);
+	return (free(temp), free(old_temp), return_value);
+}
+
 int	init_cd(t_data *data, t_lexst **list)
 {
 	char	*temp;
 	int		return_value;
 
 	*list = (*list)->next;
-	if (!(*list) || (*list)->type != BUILT_IN)
+	if (!(*list) || (*list)->type != BUILT_IN
+		|| ft_strcmp((*list)->content, "~") == 0)
 	{
 		temp = ft_getenv("HOME", data->env);
 		if (!temp)
@@ -60,5 +89,7 @@ int	init_cd(t_data *data, t_lexst **list)
 			*list = (*list)->next;
 		return (ft_printf_fd(2, "minishell: cd: too many arguments\n"), 1);
 	}
+	if (ft_strcmp((*list)->content, "-") == 0)
+		return (cd_minus(data, 0));
 	return (cd(data, (*list)->content));
 }
--- src/hardcoded/print_pwd.c
+++ src/hardcoded/print_pwd.c
@@ -6,7 +6,7 @@
 /*   By: malaimo <malaimo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 14:19:24 by malaimo           #+#    #+#             */
-/*   Updated: 2026/05/11 18:32:07 by malaimo          ###   ########.fr       */
+/*   Updated: 2026/05/11 20:13:49 by malaimo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,23 @@
 
 int	print_pwd(t_data *data, t_lexst **list)
 {
+	char	*temp;
+
 	*list = (*list)->next;
 	if (!getcwd(data->current_dir, 4096))
-		printf("%s\n", ft_getenv_gc("OLDPWD", data->env, &data->gc));
+	{
+		temp = ft_getenv_gc("OLDPWD", data->env, &data->gc);
+		if (!temp)
+		{
+			temp = ft_getenv_gc("PWD", data->env, &data->gc);
+			if (!temp)
+			{
+				printf("Minishell: error: can't access current directory");
+				return (1);
+			}
+		}
+		printf("%s\n", temp);
+	}
 	else
 		printf("%s\n", data->current_dir);
 	return (0);
